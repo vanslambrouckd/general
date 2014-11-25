8bab9ba3bb4f25b23ca4e84b98e0897983cf88d7
vanslambrouckd <vanslambrouckd@hotmail.com>
2014-11-25 19:28:09 +0100

    react test

---
+++ tests/reactjs/js/clicker.js
@@ -0,0 +1,16 @@
+var Clicker = React.createClass({
+	getInitialState: function() {
+		return { count: 0 };
+	},
+	handleClick: function() {
+		this.setState({ count: this.state.count + 1 });
+	},
+	render: function() {
+		return React.DOM.a(
+			{ onClick: this.handleClick },
+			'You have clicked ' + this.state.count + ' times'
+		);
+	}
+});
+
+React.renderComponent(Clicker(), document.getElementById('content'));
